719bcf7a9a8da52aa0801714b3d7e699cce8b881
srinidhirepala <168984935+srinidhirepala@users.noreply.github.com>
2024-09-29 23:58:44 +0530

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -1,4 +1,4 @@
-1.Create a class called Invoice that a hardware store might use to represent an invoice for an item sold  at  the  store.  An  Invoice  should  include  four  pieces  of  information  as  instance variables-a part number(type String),a part description(type String),a quantity of the item being purchased (type int) and a price per item (double). Your class should have a constructor that initializes the four instance variables. In addition, provide a method named getInvoiceAmount that calculates the invoice amount.
+//1.Create a class called Invoice that a hardware store might use to represent an invoice for an item sold  at  the  store.  An  Invoice  should  include  four  pieces  of  information  as  instance variables-a part number(type String),a part description(type String),a quantity of the item being purchased (type int) and a price per item (double). Your class should have a constructor that initializes the four instance variables. In addition, provide a method named getInvoiceAmount that calculates the invoice amount.
 
 import java.util.*;
 class Invoice
