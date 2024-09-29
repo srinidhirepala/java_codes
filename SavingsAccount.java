257715c2c3b04d7352996a32894983ec6f7e6e3c
srinidhirepala <168984935+srinidhirepala@users.noreply.github.com>
2024-09-29 23:59:18 +0530

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,4 +1,4 @@
-3.Create class SavingsAccount. Use a static variable annualInterestRate to store the annual interest rate for all account holders. Each object of the class contains a private instance variable savingsBalance indicating the amount the saver currently has ondeposit. Provide method calculateMonthlyInterest to calculate the monthly interest by multiplying the savingsBalance by annualInterestRate divided by 12 this interest  should  be  added  to  savingsBalance.  Provide  a  static  method modifyInterestRate that sets the annualInterestRate to a new value.
+//3.Create class SavingsAccount. Use a static variable annualInterestRate to store the annual interest rate for all account holders. Each object of the class contains a private instance variable savingsBalance indicating the amount the saver currently has ondeposit. Provide method calculateMonthlyInterest to calculate the monthly interest by multiplying the savingsBalance by annualInterestRate divided by 12 this interest  should  be  added  to  savingsBalance.  Provide  a  static  method modifyInterestRate that sets the annualInterestRate to a new value.
 
 
   class SavingsAccount {
