36320d6f094470374b33de4aa50b35ef1a15edf3
srinidhirepala <168984935+srinidhirepala@users.noreply.github.com>
2024-09-29 23:58:54 +0530

    Update MergeStrings.java

--- MergeStrings.java
+++ MergeStrings.java
@@ -1,4 +1,4 @@
-7.Given two strings s1 and s2, your task is to merge those strings to form a new merged string. A merge operation on two strings is described as follows: Append alternating characters from s1 and s2, respectively, to merged String. Once all of the characters in one of the strings have been merged, append the remaining characters in the other string to merged String.
+//7.Given two strings s1 and s2, your task is to merge those strings to form a new merged string. A merge operation on two strings is described as follows: Append alternating characters from s1 and s2, respectively, to merged String. Once all of the characters in one of the strings have been merged, append the remaining characters in the other string to merged String.
 
 import java.util.Scanner;
 class MergeStrings {
